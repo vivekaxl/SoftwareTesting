0a747fe11763e47f98dbfce98a63a3ccdcb3b6c1
Vivek Nair <vivekaxl@gmail.com>
2015-02-11 14:07:56 -0500

    Asdsa

--- Jan28/process.py
+++ Jan28/process.py
@@ -1,16 +1,16 @@
 """Program to process data"""
 
-def find_days(filename = "downloaded_issues.txt"):
+def find_days(filename = "processed_data.txt"):
 	with open(filename) as f:
 		before = 1419.34251881
 		for i,line in enumerate(f):
 			if i%100 == 0 and i != 0:
-				print i*100,(before -  float(line.split()[3]))
+				print i,(before -  float(line.split()[3]))
 				before = float(line.split()[3])
 
 
 
-def find_cumdays(filename = "downloaded_issues.txt"):
+def find_cumdays(filename = "processed_data.txt"):
 	with open(filename) as f:
 		before = 1419.34251881
 		for i,line in enumerate(f):
@@ -18,5 +18,5 @@ def find_cumdays(filename = "downloaded_issues.txt"):
 				print int(before - float(line.split()[3]))
 
 if __name__ == '__main__': 
-	find_cumdays()
+	#find_cumdays()
 	find_days()
--- Jan28/testpygithub3.py
+++ Jan28/testpygithub3.py
@@ -7,7 +7,7 @@ def d2s(x):
   temp =time.mktime(time.strptime(str(x), "%Y-%m-%d %H:%M:%S"))
   return (time.time() - temp)/(3600*24)
 
-auth = dict(login='vivekaxl', password='passwd')
+auth = dict(login='vivekaxl', password='wreacked9')
 gh = Github(**auth)
 
 octocat_issues = gh.issues.list_by_repo('pypa','pip',state="closed")
